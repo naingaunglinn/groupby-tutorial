46aa3beb1b559cea21aac99217fb80b8d24801ac
graywish <naingaunglinn369@gmail.com>
2024-11-24 11:38:56 +0630

    update: groupby function added

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Sale extends Model
 {
@@ -16,4 +17,8 @@ class Sale extends Model
         'quantity',
         'total_price',
     ];
+
+    public function product(): BelongsTo{
+        return $this->belongsTo(Product::class);
+    }
 }
